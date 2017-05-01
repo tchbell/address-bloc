2aedcb332e1df269eafed84d4cd3e59613ccd453
Ted <tchbell@gmail.com>
2017-05-01 19:03:38 +0000

    Added view specific entry method

--- controllers/menu_controller.rb
+++ controllers/menu_controller.rb
@@ -10,10 +10,11 @@ class MenuController
     def main_menu
         puts "Main menu - #{address_book.entries.count} entries"
         puts "1 - View all entires"
-        puts "2 - Create an entry"
-        puts "3 - Search for an entry"
-        puts "4 - Import entries from a CSV"
-        puts "5 - Exit"
+        puts "2 - View Entry Number n"
+        puts "3 - Create an entry"
+        puts "4 - Search for an entry"
+        puts "5 - Import entries from a CSV"
+        puts "6 - Exit"
         print "Enter your selection: "
         
         selection = gets.to_i
@@ -23,19 +24,24 @@ class MenuController
                 system "clear"
                 view_all_entries
                 main_menu
+                
             when 2
                 system "clear"
-                create_entry
+                view_entry_n
                 main_menu
             when 3
                 system "clear"
-                search_entries
+                create_entry
                 main_menu
             when 4
                 system "clear"
-                read_csv
+                search_entries
                 main_menu
             when 5
+                system "clear"
+                read_csv
+                main_menu
+            when 6
                 puts "Good-bye!"
                 exit(0)
             else
@@ -80,6 +86,24 @@ end
             system "clear"
             puts "End of entries"
             
+        end
+        
+        def view_entry_n
+            system "clear"
+            puts "What entry would you like to view"
+           choice = gets.chomp
+           
+           options = address_book.entries.length
+            if choice.to_i > options
+                puts "Please enter valid entry number"
+            end
+           
+           address_book.entries.each_with_index do |entry, index|
+            if choice.to_i === (index.to_i)+1
+                puts entry
+            end
+            end
+        
         end
         
         def create_entry
