d54894a57865ec8c6bd6a8729bdafca4e77f9996
Ted <tchbell@gmail.com>
2017-05-03 17:55:01 +0000

    Added new menu features: delete_entry, edit_entry, submenu

--- controllers/menu_controller.rb
+++ controllers/menu_controller.rb
@@ -10,10 +10,11 @@ class MenuController
     def main_menu
         puts "Main menu - #{address_book.entries.count} entries"
         puts "1 - View all entires"
-        puts "2 - Create an entry"
-        puts "3 - Search for an entry"
-        puts "4 - Import entries from a CSV"
-        puts "5 - Exit"
+        puts "2 - View Entry Number n"
+        puts "3 - Create an entry"
+        puts "4 - Search for an entry"
+        puts "5 - Import entries from a CSV"
+        puts "6 - Exit"
         print "Enter your selection: "
         
         selection = gets.to_i
@@ -23,19 +24,24 @@ class MenuController
                 system "clear"
                 view_all_entries
                 main_menu
+                
             when 2
                 system "clear"
-                create_entry
+                view_entry_n
                 main_menu
             when 3
                 system "clear"
-                search_entries
+                create_entry
                 main_menu
             when 4
                 system "clear"
-                read_csv
+                search_entries
                 main_menu
             when 5
+                system "clear"
+                read_csv
+                main_menu
+            when 6
                 puts "Good-bye!"
                 exit(0)
             else
@@ -58,7 +64,10 @@ class MenuController
         
         when "n"
         when "d"
+            delete_entry(entry)
         when "e"
+            edit_entry(entry)
+            entry_submenu(entry)
         when "m"
             system "clear"
             main_menu
@@ -82,6 +91,24 @@ end
             
         end
         
+        def view_entry_n
+            system "clear"
+            puts "What entry would you like to view"
+           choice = gets.chomp
+           
+           options = address_book.entries.length
+            if choice.to_i > options
+                puts "Please enter valid entry number"
+            end
+           
+           address_book.entries.each_with_index do |entry, index|
+            if choice.to_i === (index.to_i)+1
+                puts entry
+            end
+            end
+        
+        end
+        
         def create_entry
             system "clear"
             puts "New AddressBloc Entry"
@@ -99,9 +126,91 @@ end
         end
         
         def search_entries
+            print "Search by name: "
+            name = gets.chomp
+            
+            match = address_book.binary_search(name)
+            system "clear"
+            
+            if match
+                puts match.to_s
+                search_submenu(match)
+            else
+                puts "No match found for #{name}"
+            end
         end
         
         def read_csv
+            print "Enter CSV file to import: "
+            file_name = gets.chomp
+            
+            if file_name.empty?
+                system "clear"
+                puts "No CSV file read"
+                main_menu
+            end
+            
+            begin 
+                entry_count = address_book.import_from_csv(file_name).count
+                system "clear"
+                puts "#{entry_count} new entries added from #{file_name}"
+            rescue
+                puts "#{file_name} is not a valid CSV file, please enter the name of a valid CSV file"
+                read_csv
+            end
+            
+            
+            def delete_entry(entry)
+                address_book.entries.delete(entry)
+                puts "#{entry.name} has been deleted"
+            end
+            
+            
+            def edit_entry(entry)
+                print "Updated Name: "
+                name = gets.chomp
+                print "Updated phone number: "
+                phone_number = gets.chomp
+                print "Updated email: "
+                email = gets.chomp
+                
+                entry.name = name if !name.empty?
+                entry.phone_number = phone_number if !phone_number.empty?
+                entry.email = email if !email.empty?
+                system "clear"
+                
+                puts "Updated entry:"
+                puts entry
+            end
+            
+            def search_submenu(entry)
+                puts "\nd - delete entry"
+                puts "e - edit this entry"
+                puts "m - return to main menu"
+                selection = gets.chomp
+                
+                case selection
+                    when "d"
+                        system"clear"
+                        delete_entry(entry)
+                        main_menu
+                    when "e"
+                        edit_entry(entry)
+                        system "clear"
+                        main_menu
+                    when "m"
+                        system "clear"
+                        main_menu
+                    else
+                        system "clear"
+                        puts "#{selection} is not a valid input"
+                        puts entry.to_s
+                        search_submenu(entry)
+                end
+            end
+                        
+                        
+            
         end
         
         
